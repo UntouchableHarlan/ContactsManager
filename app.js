13a128a08113877ea1383c34f73199ed8889a4e8
UntouchableHarlan <salomonpluviose1@gmail.com>
2020-01-15 12:57:21 -0500

    added ejs and also implemented login page with template for sign in page and landing page

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+// const firebase = require("firebase");
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render('landingPage');
+});
+
+app.get('/signup', function (req, res) {
+  res.render('signup');
 })
+
+app.get('/login', function (req, res) {
+  res.render('login');
+});
 
 app.listen(3000, () => {
   console.log(`App listening on port 3000`);
