ebce1bf4f63762ee08917630907f20520333d14a
Schnieder Maxime <schniedermaxime@Schnieders-MacBook-Air.local>
2020-01-29 14:44:32 -0500

    created create page and now users can dynamically create contacts within the app

--- middleware/index.js
+++ middleware/index.js
@@ -3,18 +3,22 @@ const firebase = require("firebase");
 module.exports = {
   isAuth: function(req, res, next) {
     firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log("User is signed in");
-        // req.session.userId = user.uid;
-        return next();
-      } else {
-        console.log(req.session);
-        console.log("User is signed out");
-        return res.redirect('/login');
+      try {
+        if (user) {
+          console.log("User is signed in");
+          console.log(user.uid);
+          req.session.userId = user.uid;
+          return next();
+        } else {
+          console.log(req.session);
+          console.log("User is signed out");
+          return res.redirect('/login');
+        }
+      } catch (error) {
+        console.log(`error: ${error}`)
       }
     });
     // next();
   }
-
 
 };
